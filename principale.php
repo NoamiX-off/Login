e5e4813aac542dd91468fac6f7a0466b4bf8f97f
NoamiX-off <70038972+NoamiX-off@users.noreply.github.com>
2020-12-05 23:09:53 +0000

    principale.php

--- principale.php
+++ principale.php
@@ -1,4 +1,3 @@
-
 <html>
     <head>
         <meta charset="utf-8">
@@ -7,13 +6,24 @@
     </head>
     <body style='background:#fff;'>
         <div id="content">
+            
+            <a href='principale.php?deconnexion=true'><span>Déconnexion</span></a>
+            
             <!-- tester si l'utilisateur est connecté -->
             <?php
                 session_start();
-                if($_SESSION['username'] !== ""){
+                if(isset($_GET['deconnexion']))
+                { 
+                   if($_GET['deconnexion']==true)
+                   {  
+                      session_unset();
+                      header("location:login.php");
+                   }
+                }
+                else if($_SESSION['username'] !== ""){
                     $user = $_SESSION['username'];
                     // afficher un message
-                    echo "Bonjour $user, vous êtes connecté";
+                    echo "<br>Bonjour $user, vous êtes connectés";
                 }
             ?>
             
